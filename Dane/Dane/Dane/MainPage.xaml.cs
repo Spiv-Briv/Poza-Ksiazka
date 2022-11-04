4c6b8985b1b40c8712593b6a6c46648484bb11d8
Spiv Briv <bebenekdawid2137@gmail.com>
2022-11-04 09:41:58 +0100

    Dane

---
+++ Dane/Dane/Dane/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Management;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace Dane
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox Outp = new TextBox();
+            Outp.BorderBrush = new SolidColorBrush(Windows.UI.Color.FromArgb(255, 255, 255, 255));
+            Outp.BorderThickness = new Thickness(1);
+            Outp.IsReadOnly = true;
+            Person Osoba = new Person();
+            if (this.data.SelectedDate == null)
+            {
+                Osoba.SetData(this.Imie.Text, this.Nazwisko.Text, null);
+            }
+            else
+            {
+                byte wiek = (byte)(2022 - this.data.SelectedDate.Value.Year);
+                Osoba.SetData(this.Imie.Text, this.Nazwisko.Text, wiek);
+            }
+            Outp.Text = Osoba.GetData();
+            this.Output.Children.Add(Outp);
+            this.Imie.Text = "";
+            this.Nazwisko.Text = "";
+            this.data.SelectedDate = null;
+        }
+    }
+}
---
+++ Dane/Dane/Dane/Person.cs
@@ -0,0 +1,64 @@
+﻿//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using Windows.ApplicationModel.Store.Preview.InstallControl;
+
+namespace Dane
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private string FirstName { get; set; }
+        private byte? Wiek { get; set; }
+        public Person(string Imie, string Nazwisko)
+        {
+            this.FirstName = Imie;
+            this.Name = Nazwisko;
+            this.Wiek = null;
+        }
+        public Person(string Imie, string Nazwisko, byte Wiek)
+        {
+            this.FirstName = Imie;
+            this.Name = Nazwisko;
+            this.Wiek = Wiek;
+        }
+        public Person() { }
+        public void SetData(string Imie, string Nazwisko, byte? Wiek)
+        {
+            this.FirstName = Imie;
+            this.Name = Nazwisko;
+            this.Wiek = Wiek;
+        }
+        public string GetData()
+        {
+            if (this.FirstName == "" || this.Name == "")
+            {
+                return "Należy uzupełnić dane";
+            }
+            else
+            {
+                return this.FirstName + " " + this.Name + " " + WiekText();
+            }
+        }
+        private string WiekText()
+        {
+            if (this.Wiek == null)
+            {
+                return "(Wiek nieznany)";
+            }
+            else
+            {
+                return "(" + this.Wiek + " l.)";
+            }
+        }
+        public override string ToString()
+        {
+            return "Person:{Constrctors:[();(string, string);(string,string,byte)],Variables:[FirstName: \"" + this.FirstName + "\", Name: \"" + this.Name + "\", Wiek: \"" + this.Wiek + "\"], Methods:[void SetData(string, string, byte?)), string GetData()]}";
+        }
+        public string Klasa()
+        {
+            return "Dostępne metody: setData(string, string, byte)/*Nadaje wartość Imienia, Nazwiska oraz Wieku (opcjonalnie)*/, GetData()/*Zwraca w postaci string Imię, Nazwisko i Wiek*/";
+        }
+    }
+}
