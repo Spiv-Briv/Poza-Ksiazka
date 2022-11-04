a66f6522fd2deb222fea7b239be3c1c238ea5116
Spiv Briv <bebenekdawid2137@gmail.com>
2022-11-04 09:51:42 +0100

    Mapa

---
+++ Mapa/Mapa/Mapa/MainPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//Szablon elementu Pusta strona jest udokumentowany na stronie https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x415
+
+namespace Mapa
+{
+    /// <summary>
+    /// Pusta strona, która może być używana samodzielnie lub do której można nawigować wewnątrz ramki.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Miasto[] City = {
+            null,
+            new Miasto(1, "Gorzów Wlkp.", "GOR", 0, 0),
+            new Miasto(2, "Klodawa", "KLO", -16, -64),
+            new Miasto(3, "Santok", "SAN", 117, -6),
+            new Miasto(4, "Nowiny Wlk.","NOW",-154,66),
+            new Miasto(5, "Deszczno", "DES", 46,66),
+            new Miasto(6, "Strzelce Kraj.", "STR", 195, -160),
+            new Miasto(7, "Krzeszyce", "KRE", -155, 161),
+            new Miasto(8, "Kostrzyn n/O.", "KOS", -392, 168),
+            new Miasto(9, "Skwierzyna", "SKW", 179, 154),
+            new Miasto(10, "Dębno", "DEB", -364, -5)
+            //new Miasto(, "Baczyna", "BAC", )
+        };
+        private int[,] pol = {
+            {0,0,0,0 },
+            {2, 4, 5, 3},
+            {0, 10, 1, 6 },
+            {6, 1, 9, 0 },
+            {10, 8, 7, 1 },
+            {1, 4, 9, 7 },
+            {0, 2, 3, 0 },
+            {4, 8, 9, 5 },
+            {10, 0, 0, 7 },
+            {5, 7, 0, 3 },
+            {0, 0, 8, 2 }
+        };
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            for (int i = 1; i < City.Length; i++)
+            {
+                Mapa.Children.Add(City[i].Pokaz());
+
+
+            }
+            for (int i = 1; i < pol.Length / 4; i++)
+            {
+                City[i].Drogi(City[pol[i, 0]], City[pol[i, 1]], City[pol[i, 2]], City[pol[i, 3]]);
+                Mapa.Children.Add(City[i].Polacz(0));
+                Mapa.Children.Add(City[i].Polacz(1));
+                Mapa.Children.Add(City[i].Polacz(2));
+                Mapa.Children.Add(City[i].Polacz(3));
+
+            }
+        }
+    }
+}
---
+++ Mapa/Mapa/Mapa/Miasto.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace Mapa
+{
+    internal class Miasto
+    {
+        string Name;
+        string Shortname;
+        int ID;
+        int X;
+        int Y;
+        Miasto[] Polo = new Miasto[4];
+        public Miasto(int ID, string Name, string Shortname, int X, int Y)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Shortname = Shortname;
+            this.X = X;
+            this.Y = Y;
+        }
+        public Miasto(int ID, string Name, string Shortname, int X, int Y, Miasto N)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Shortname = Shortname;
+            this.X = X;
+            this.Y = Y;
+            this.Polo[0] = N;
+        }
+        public Miasto(int ID, string Name, string Shortname, int X, int Y, Miasto N, Miasto W)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Shortname = Shortname;
+            this.X = X;
+            this.Y = Y;
+            this.Polo[0] = N;
+            this.Polo[1] = W;
+        }
+        public Miasto(int ID, string Name, string Shortname, int X, int Y, Miasto N, Miasto W, Miasto S)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Shortname = Shortname;
+            this.X = X;
+            this.Y = Y;
+            this.Polo[0] = N;
+            this.Polo[1] = W;
+            this.Polo[2] = S;
+        }
+        public Miasto(int ID, string Name, string Shortname, int X, int Y, Miasto N, Miasto W, Miasto S, Miasto E)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Shortname = Shortname;
+            this.X = X;
+            this.Y = Y;
+            this.Polo[0] = N;
+            this.Polo[1] = W;
+            this.Polo[2] = S;
+            this.Polo[3] = E;
+        }
+        public void Drogi(Miasto A, Miasto B, Miasto C, Miasto D)
+        {
+            this.Polo[0] = A;
+            this.Polo[1] = B;
+            this.Polo[2] = C;
+            this.Polo[3] = D;
+        }
+        public TextBlock Pokaz()
+        {
+            TextBlock block = new TextBlock();
+            block.Text = Name;
+            block.HorizontalAlignment = Windows.UI.Xaml.HorizontalAlignment.Left;
+            block.HorizontalTextAlignment = Windows.UI.Xaml.TextAlignment.Center;
+            block.VerticalAlignment = Windows.UI.Xaml.VerticalAlignment.Top;
+            block.Width = 100;
+            block.Height = 20;
+            block.Margin = new Windows.UI.Xaml.Thickness(500 - (block.Width / 2) + X, 300 - (block.Height) + Y, 0, 0);
+            block.Foreground = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+
+            return block;
+        }
+        public Line Polacz(int i)
+        {
+            Line linia = new Line();
+            linia.X1 = 0;
+            linia.X2 = 0;
+            linia.Y1 = 0;
+            linia.Y2 = 0;
+            if (Polo[i] != null)
+            {
+                //Line linia = new Line();
+                int roznicaX = Polo[i].X - this.X;
+                int roznicaY = Polo[i].Y - this.Y;
+                if (roznicaX < 0) roznicaX *= -1;
+                if (roznicaY < 0) roznicaY *= -1;
+                if (roznicaX > roznicaY)
+                {
+                    if (this.X < Polo[i].X)
+                    {
+                        linia.X1 = this.X + 500;
+                        linia.X2 = Polo[i].X + 500;
+                        linia.Y1 = this.Y + 290;
+                        linia.Y2 = Polo[i].Y + 290;
+                    }
+                    if (this.X > Polo[i].X)
+                    {
+                        linia.X1 = Polo[i].X + 500;
+                        linia.X2 = this.X + 500;
+                        linia.Y1 = Polo[i].Y + 290;
+                        linia.Y2 = this.Y + 290;
+                    }
+                }
+                else
+                {
+                    if (this.Y < Polo[i].Y)
+                    {
+                        linia.X1 = this.X + 500;
+                        linia.X2 = Polo[i].X + 500;
+                        linia.Y1 = this.Y + 290;
+                        linia.Y2 = Polo[i].Y + 290;
+                    }
+                    if (this.Y > Polo[i].Y)
+                    {
+                        linia.X1 = this.X + 500;
+                        linia.X2 = Polo[i].X + 500;
+                        linia.Y1 = this.Y + 290;
+                        linia.Y2 = Polo[i].Y + 290;
+                    }
+                }
+                linia.Stroke = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+                linia.StrokeThickness = 1;
+                return linia;
+            }
+            else
+            {
+                return linia;
+            }
+        }
+    }
+}
